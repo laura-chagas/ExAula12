c3a6f39b303eb3857d94c98c38a4f3a69185b5d4
laura.chagas <laura.chagas@zup.com.br>
2023-04-27 17:23:37 -0300

    Upload Exercicios de Laço de Repetição e Vetores via IntelliJ

---
+++ src/LRepeticao/ExTres.java
@@ -0,0 +1,15 @@
+package LRepeticao;
+
+
+public class ExTres {
+
+    public static void main(String[] args) {
+
+        for (int i = 101; i <= 110; i++ ){
+            System.out.println(i);
+        }
+
+    }
+
+
+}
